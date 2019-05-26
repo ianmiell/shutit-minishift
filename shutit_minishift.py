1463b8e7225c476dc872b2e26d1037146c44fd2f
ian <ian.miell@gmail.com>
2019-05-26 18:26:06 +0100

    linux

--- shutit_minishift.py
+++ shutit_minishift.py
@@ -22,11 +22,20 @@ class shutit_minishift(ShutItModule):
 			shutit.send('sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve')
 			shutit.send('sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve')
 			shutit.logout()
+			shutit.send('brew cask install --force minishift || brew cast upgrade --force minishift')
+			shutit.send('minishift start --cpus 4 --memory 18GB')
+			shutit.send('minishift profile set minishift')
+			if shutit.send_and_get_output('''minishift status  | grep ^Minishift | awk {'print $2'}''') == 'Running':
+				shutit.pause_point('minishift already running, either CTRL-] to continue with it, or CTRL-Q to quit and run minishift delete to remove before re-running')
 		elif platform == 'Linux':
 			# See: https://www.novatec-gmbh.de/en/blog/getting-started-minishift-openshift-origin-one-vm/
 			# Going to assume it's already there.
 			if not shutit.command_available('minishift'):
 				shutit.pause_point('Need to install minishift, see: https://www.novatec-gmbh.de/en/blog/getting-started-minishift-openshift-origin-one-vm/')
+			shutit.send('minishift start --cpus 4 --memory 12GB --vm-driver virtualbox')
+			shutit.send('minishift profile set minishift')
+			if shutit.send_and_get_output('''minishift status  | grep ^Minishift | awk {'print $2'}''') == 'Running':
+				shutit.pause_point('minishift already running, either CTRL-] to continue with it, or CTRL-Q to quit and run minishift delete to remove before re-running')
 		if not shutit.command_available('oc'):
 			shutit.send('wget -nv https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-mac.zip')
 			shutit.multisend('unzip openshift-origin-client-tools-v3.11.0-0cbc58b-mac.zip',{'replace':'y'})
@@ -37,11 +46,6 @@ class shutit_minishift(ShutItModule):
 			shutit.send('rm ' + wd + '/openshift-origin-client-tools-v3.11.0-0cbc58b-mac.zip')
 			shutit.logout()
 			shutit.pause_point('')
-		shutit.send('brew cask install --force minishift || brew cast upgrade --force minishift')
-		shutit.send('minishift profile set minishift')
-		if shutit.send_and_get_output('''minishift status  | grep ^Minishift | awk {'print $2'}''') == 'Running':
-			shutit.pause_point('minishift already running, either CTRL-] to continue with it, or CTRL-Q to quit and run minishift delete to remove before re-running')
-		shutit.send('minishift start --cpus 4 --memory 18GB')
 		shutit.send('eval $(minishift oc-env)')
 		minishift_ip = shutit.send_and_get_output('minishift ip')
 		shutit.pause_point('\n\nminishift ip is: ' + minishift_ip + '\n\n... go to: \n\nhttps://' + minishift_ip + ':8443/console\n\nto see the console')
