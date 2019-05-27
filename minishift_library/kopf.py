62521536bb2953bf417653b99cb0addcad8a9f6e
ian <ian.miell@gmail.com>
2019-05-27 18:29:08 +0100

    ws

--- minishift_library/kopf.py
+++ minishift_library/kopf.py
@@ -13,30 +13,30 @@ def do_kopf(s):
 	s.send('mkdir walkthrough && cd walkthrough')
 	# Create the CRD for the evc (ephemeralvolumeclaim)
 	s.send_file('crd.yaml',''' apiVersion: apiextensions.k8s.io/v1beta1
- kind: CustomResourceDefinition
- metadata:
-   name: ephemeralvolumeclaims.zalando.org
- spec:
-   scope: Namespaced
-   group: zalando.org
-   versions:
-     - name: v1
-       served: true
-       storage: true
-   names:
-     kind: EphemeralVolumeClaim
-     plural: ephemeralvolumeclaims
-     singular: ephemeralvolumeclaim
-     shortNames:
-       - evcs
-       - evc''')
+kind: CustomResourceDefinition
+metadata:
+  name: ephemeralvolumeclaims.zalando.org
+spec:
+  scope: Namespaced
+  group: zalando.org
+  versions:
+    - name: v1
+      served: true
+      storage: true
+  names:
+    kind: EphemeralVolumeClaim
+    plural: ephemeralvolumeclaims
+    singular: ephemeralvolumeclaim
+    shortNames:
+      - evcs
+      - evc''')
 	s.send('kubectl apply -f crd.yaml')
 
 	# Create an evc object
 	s.send_file('obj.yaml',''' apiVersion: zalando.org/v1
- kind: EphemeralVolumeClaim
- metadata:
-   name: my-claim''')
+kind: EphemeralVolumeClaim
+metadata:
+  name: my-claim''')
 	s.send('kubectl apply -f obj.yaml')
 	s.send('kubectl get EphemeralVolumeClaim')
 	s.send('kubectl get ephemeralvolumeclaims')
@@ -45,30 +45,30 @@ def do_kopf(s):
 	# Now create supid-simple operator.
 	s.send_file('ephemeral.py','''import kopf
 
- @kopf.on.create('zalando.org', 'v1', 'ephemeralvolumeclaims')
- def create_fn(body, **kwargs):
-     print(f"A handler is called with body: {body}")''')
+@kopf.on.create('zalando.org', 'v1', 'ephemeralvolumeclaims')
+def create_fn(body, **kwargs):
+    print(f"A handler is called with body: {body}")''')
 	s.pause_point('now run: kopf run ephemeral.py --verbose &, then in another terminal oc delete evc and watch it handle.')
 	# Create a new claim
 	s.send_file('evc.yaml',''' apiVersion: zalando.org/v1
- kind: EphemeralVolumeClaim
- metadata:
-   name: my-claim
- spec:
-   size: 10G''')
+kind: EphemeralVolumeClaim
+metadata:
+  name: my-claim
+spec:
+  size: 10G''')
 	# Create templated pvc yaml file
 	s.send_file('pvc.yaml',''' apiVersion: v1
- kind: PersistentVolumeClaim
- metadata:
-   name: "{name}"
-   annotations:
-     volume.beta.kubernetes.io/storage-class: standard
- spec:
-   accessModes:
-     - ReadWriteOnce
-   resources:
-     requests:
-       storage: "{size}"''')
+kind: PersistentVolumeClaim
+metadata:
+  name: "{name}"
+  annotations:
+    volume.beta.kubernetes.io/storage-class: standard
+spec:
+  accessModes:
+    - ReadWriteOnce
+  resources:
+    requests:
+      storage: "{size}"''')
 
 	# Create new handler
 	s.send_file('ephemeral.py','''import kopf
